e8f8c2515a31def88fa44bc5951db634abdfbefc
destiny127 <74096993+destiny127@users.noreply.github.com>
2022-06-12 09:29:15 +0000

    Added main file

---
+++ main.py
@@ -0,0 +1,22 @@
+from mega import Mega
+from datetime import datetime
+import os
+import time
+
+m = Mega()
+usr = m.login("uploadme@yopmail.com","Bhaibhai45")
+
+while True:
+
+        time.sleep(5)
+
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        fname = "Time " + str(current_time) + ".txt"
+
+        with open(fname,"w") as f:
+                f.write(current_time)
+
+        usr.upload(fname)
+        print(":: Uploaded file !")
+        os.remove(fname)
